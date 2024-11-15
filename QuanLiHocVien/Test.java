c2dbc62cddf467b39a7af3512789dca8cdc42d47
NguyenCongVinh1717 <nguyencongvinhbv2004@gmail.com>
2024-11-15 22:15:51 +0700

    upload

--- QuanLiHocVien/Test.java
+++ QuanLiHocVien/Test.java
@@ -3,6 +3,7 @@ package Chuong3.QuanLiHocVien;
 
 public class Test {
     public static void main(String[] args) {
+//Khoi tao 1 doi tuong
         HocVienLT n1=new HocVienLT();
         n1.nhapThongTin();
         n1.inThongTin();
